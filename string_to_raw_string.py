dd5c8f33b043d2c10e52a72691b9b54d438f9c6c
Tomas Orihuela <tomas.orihuela@gmail.com>
2022-03-12 08:25:54 -0600

    Found function that handles converting windows paths to usable string

--- string_to_raw_string.py
+++ string_to_raw_string.py
@@ -1,31 +1,39 @@
 #
 # convert a string to a raw string
-# BUT, doesn't work overly well when  copying and pasting
+# BUT, doesn't work overly well when copying and pasting
 # MS Windows paths.
 #
 
 
-# 'string' to r'string'
-# this works since nothing could be intrepret as an escape char
-d = 'D:\Projects\Miscellaneous\Testing'
-mydir = r'{}'.format(d)    #convert to a raw string
-print(mydir)
+def raw(text):
+    """Returns a raw string representation of text
 
+    src: https://code.activestate.com/recipes/65211/
+    """
 
-# 'string' to r'string' but with replacement since the directory that
-# starts with a 0. Replace needed since \0 is intrepreted as an escape char.
-# Note, other replacements would be needed for other escape code.
-# For example if folder starts with a t, you'd have \t that is Interrupted
-# as an escape code for a tab
-d = 'D:\Projects\Miscellaneous\0-testing'
-d = d.replace('\0','\\0')   # convert files
-mydir = r'{}'.format(d)    #convert to a raw string
-print(mydir)
+    escape_dict={'\a':r'\a',
+               '\b':r'\b',
+               '\c':r'\c',
+               '\f':r'\f',
+               '\n':r'\n',
+               '\r':r'\r',
+               '\t':r'\t',
+               '\v':r'\v',
+               '\'':r'\'',
+               '\"':r'\"',
+               '\0':r'\0',
+               '\1':r'\1',
+               '\2':r'\2',
+               '\3':r'\3',
+               '\4':r'\4',
+               '\5':r'\5',
+               '\6':r'\6',
+               '\7':r'\7',
+               '\8':r'\8',
+               '\9':r'\9'}
 
-
-
-# you have no issues if you just use a raw string from the get go
-d = r'D:\Projects\Miscellaneous\testing'
-print(d)
-d = r'D:\Projects\Miscellaneous\0-testing'
-print(d)
+    new_string=''
+    for char in text:
+        try: new_string+=escape_dict[char]
+        except KeyError: new_string+=char
+    return new_string
